3510cb6ffdf1a9d7be5bbb3d2c95b9633201630e
dreadwing5 <sachinspyk@outlook.com>
2024-12-23 01:22:03 +0530

    Add Image Upload API

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -16,6 +16,8 @@ namespace Application.Activities
 
         public bool IsCancelled { get; set; }
 
-        public ICollection<Profile> Attendees { get; set; }
+
+
+        public ICollection<AttendeeDto> Attendees { get; set; }
     }
 }
